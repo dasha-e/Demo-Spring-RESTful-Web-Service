9772808321c24adc3cb79c2984f19eddaaa15746
dasha-e <epimahova.ds@edu.spbstu.ru>
2022-06-22 01:14:26 +0300

    GET POST PUT DELETE endpoints

--- src/main/java/com/example/demo/controller/AlgorithmController.java
+++ src/main/java/com/example/demo/controller/AlgorithmController.java
@@ -5,16 +5,32 @@ import com.example.demo.service.AlgorithmService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class AlgorithmController {
-    //localhost:8080/Algorithm
+    //localhost:8080/
     private final AlgorithmService algorithmService;
 
-    @RequestMapping(value = "/algorithm", method = RequestMethod.GET)
-    public Optional<Algorithm> getAlgorithm() {
-        return algorithmService.getAlgorithm();
+    @RequestMapping(value = "/get-algorithm", method = RequestMethod.GET)
+    public List<Algorithm> getAllAlgorithms() {
+        return algorithmService.getAllAlgorithms();
     }
+
+    @RequestMapping(value = "/insert-algorithm", method = RequestMethod.POST)
+    public void createAlgorithm(@RequestBody Algorithm algorithm) {
+        algorithmService.saveAlgorithm(algorithm);
+    }
+
+    @RequestMapping(value = "/update-algorithm", method = RequestMethod.PUT)
+    public void updateAlgorithm(@RequestBody Algorithm algorithm) {
+        algorithmService.updateAlgorithm(algorithm);
+    }
+
+    @RequestMapping(value = "/delete-algorithm", method = RequestMethod.DELETE)
+    public void deleteAlgorithm(Long id) {
+        algorithmService.deleteAlgorithm(id);
+    }
+
 }
