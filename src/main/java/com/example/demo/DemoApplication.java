0cd81b0cffae518bef7cfb2987948a9d5ce453e9
dasha-e <epimahova.ds@edu.spbstu.ru>
2022-06-22 12:05:44 +0300

    Swagger comments

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Algorithm API", version = "1.0", description = "Demo RESTful Web Service connected witn postgres database"))
 @SpringBootApplication
 public class DemoApplication {
 
