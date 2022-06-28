404ca8497f6ad2d451c49a58843f850b3b8fc98a
dasha-e <epimahova.ds@edu.spbstu.ru>
2022-06-29 00:33:25 +0300

    Error controller, response status is 500

---
+++ src/main/java/com/example/demo/DTO/ExceptionDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ExceptionDTO {
+    private final String message;
+    private final HttpStatus httpStatus;
+}
