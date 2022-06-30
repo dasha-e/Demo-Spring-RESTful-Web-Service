a4eddf7bfa07dbd892ec7ec90522d7f34594429d
dasha-e <epimahova.ds@edu.spbstu.ru>
2022-07-01 00:26:14 +0300

    Sorting tests changes

--- src/test/java/com/example/demo/BubbleSortTest.java
+++ src/test/java/com/example/demo/BubbleSortTest.java
@@ -3,47 +3,22 @@ package com.example.demo;
 import com.example.demo.service.algorithm.BubbleSort;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class BubbleSortTest {
+class BubbleSortTest extends SortingTest {
 
     BubbleSort sort = new BubbleSort();
 
     @Test
     public void sort_emptyArray() {
-        int[] arr = new int[0];
-        sort.sort(arr);
-        assertEquals(Arrays.toString(new int[0]), Arrays.toString(arr));
+        sort_emptyArray(sort);
     }
 
     @Test
     public void sort_sortedArray() {
-        int[] arr = new int[]{1};
-        sort.sort(arr);
-        assertEquals(Arrays.toString(new int[]{1}), Arrays.toString(arr));
-        arr = new int[]{1, 2, 2, 4};
-        sort.sort(arr);
-        assertEquals(Arrays.toString(new int[]{1, 2, 2, 4}), Arrays.toString(arr));
-        arr = new int[]{2, 2, 2, 2};
-        sort.sort(arr);
-        assertEquals(Arrays.toString(new int[]{2, 2, 2, 2}), Arrays.toString(arr));
+        sort_sortedArray(sort);
     }
 
     @Test
-    public void sort() {
-        int[] arr = new int[]{1, 0, 10, 3, 2};
-        sort.sort(arr);
-        assertEquals(Arrays.toString(new int[]{0, 1, 2, 3, 10}), Arrays.toString(arr));
-        arr = new int[]{1, 10, 1, 10};
-        sort.sort(arr);
-        assertEquals(Arrays.toString(new int[]{1, 1, 10, 10}), Arrays.toString(arr));
-        arr = new int[]{4, 3, 2, 1};
-        sort.sort(arr);
-        assertEquals(Arrays.toString(new int[]{1, 2, 3, 4}), Arrays.toString(arr));
-        arr= new int[]{10, 10, 2, 45, 4};
-        sort.sort(arr);
-        assertEquals(Arrays.toString(new int[]{2, 4, 10, 10, 45}), Arrays.toString(arr));
+    public void sort_ordinaryArray() {
+        sort_ordinaryArray(sort);
     }
 }
