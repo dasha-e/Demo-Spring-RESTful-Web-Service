9f9faf9e728343c5c6aca97e18fc2d889fb170f4
dasha-e <epimahova.ds@edu.spbstu.ru>
2022-07-05 20:38:15 +0300

    SortingTest changes

--- src/test/java/com/example/demo/SortingTest.java
+++ src/test/java/com/example/demo/SortingTest.java
@@ -1,48 +1,50 @@
 package com.example.demo;
 
 import com.example.demo.service.algorithm.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SortingTest {
+    static private Map<String, SortingStrategy> sortingStrategy = new HashMap<>();
+
+    @BeforeAll
+    public static void SortingStrategy() {
+        sortingStrategy.put("1", new BubbleSort());
+        sortingStrategy.put("2", new SelectionSort());
+        sortingStrategy.put("3", new InsertionSort());
+        sortingStrategy.put("4", new QuickSort());
+        sortingStrategy.put("5", new MergeSort());
+        sortingStrategy.put("6", new HeapSort());
+        sortingStrategy.put("7", new TreeSort());
+        sortingStrategy.put("8", new AVLTreeSort());
+    }
 
     @Test
     public void sort_emptyArray_test() {
-        sort_emptyArray(new BubbleSort());
-        sort_emptyArray(new SelectionSort());
-        sort_emptyArray(new InsertionSort());
-        sort_emptyArray(new QuickSort());
-        sort_emptyArray(new MergeSort());
-        sort_emptyArray(new HeapSort());
-        sort_emptyArray(new TreeSort());
-        sort_emptyArray(new AVL_TreeSort());
+        for (int i = 1; i < sortingStrategy.size(); i++) {
+            sort_emptyArray(sortingStrategy.get(Integer.toString(i)));
+        }
     }
 
     @Test
     public void sort_sortedArray_test() {
-        sort_sortedArray(new BubbleSort());
-        sort_sortedArray(new SelectionSort());
-        sort_sortedArray(new InsertionSort());
-        sort_sortedArray(new QuickSort());
-        sort_sortedArray(new MergeSort());
-        sort_sortedArray(new HeapSort());
-        sort_sortedArray(new TreeSort());
-        sort_sortedArray(new AVL_TreeSort());
+        for (int i = 1; i < sortingStrategy.size(); i++) {
+            sort_sortedArray(sortingStrategy.get(Integer.toString(i)));
+        }
     }
 
     @Test
     public void sort_ordinaryArray_test() {
-        sort_ordinaryArray(new BubbleSort());
-        sort_ordinaryArray(new SelectionSort());
-        sort_ordinaryArray(new InsertionSort());
-        sort_ordinaryArray(new QuickSort());
-        sort_ordinaryArray(new MergeSort());
-        sort_ordinaryArray(new HeapSort());
-        sort_ordinaryArray(new TreeSort());
-        sort_ordinaryArray(new AVL_TreeSort());
+        for (int i = 1; i < sortingStrategy.size(); i++) {
+            sort_ordinaryArray(sortingStrategy.get(Integer.toString(i)));
+        }
     }
 
     private void sort_emptyArray(SortingStrategy sort) {
